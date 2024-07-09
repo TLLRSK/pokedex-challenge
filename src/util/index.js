b4c4da2a968a113035208a370c579e2134fa3388
TLLRSK <tullarisk@gmail.com>
2024-07-09 18:27:38 +0200

    change: exporting files from index.js

---
+++ src/util/index.js
@@ -0,0 +1,7 @@
+export { default as LightToggler } from '../components/LightToggler.vue';
+export { default as Nav } from '../components/Nav.vue';
+export { default as TopBar } from '../components/TopBar.vue';
+export { default as Pagination } from '../components/Pagination.vue';
+export { default as PokemonCard } from '../components/PokemonCard.vue';
+export { default as PokemonList } from '../components/PokemonList.vue';
+export { default as ViewToggler } from '../components/ViewToggler.vue';
