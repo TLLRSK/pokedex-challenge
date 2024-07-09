481f1e3b8b7c33a8093df939014ba270dcf83da7
TLLRSK <tullarisk@gmail.com>
2024-07-09 21:35:33 +0200

    add: fetching pokemon data, passing them as prop and render in PokemonList

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <top-bar/>
   <main>
     <view-toggler/>
-    <pokemon-list/>
+    <pokemon-list :pokedexData="this.pokedexData"/>
     <pokemon-card/>
   </main>
   <pagination/>
@@ -24,6 +24,40 @@ export default {
     Pagination,
     ViewToggler,
   },
+  data() {
+    return {
+      pokedexData: [],
+    }
+  },
+  methods: {
+    async fetchData() {
+      const baseUrl = "https://pokeapi.co/api/v2/pokedex/6/"
+      try {
+        const data = await fetch(baseUrl);
+        const response = await data.json();
+
+        const allPokemonData = await Promise.all(response.pokemon_entries.map(async(pokemonEntry) => {
+          const pokemonUrl = await pokemonEntry.pokemon_species.url;
+          const splittedId = pokemonUrl.split('/').filter(blank => blank).pop()
+          const url = `https://pokeapi.co/api/v2/pokemon/${splittedId}`
+          try {
+            const data = await fetch(url);
+            const response = await data.json();
+            return response;
+          } catch(error) {
+            throw error;
+          }
+        }))
+        
+        this.pokedexData = allPokemonData;
+      } catch (error) {
+        throw error;
+      }
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
 
 }
 </script>
--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,8 +1,24 @@
 <template>
-    <div>Pokemon List</div>
+    <ul>
+        <li v-for="(pokemon, index) in pokedexData" :key="index">
+            <img :src="pokemon.sprites.front_default" alt="">
+            <span>{{pokemon.id}}</span>
+            <span>{{pokemon.name}}</span>
+            <div>
+                <span v-for="type in pokemon.types" :key="type">
+                {{type.type.name}}
+                </span>
+            </div>
+        </li>
+    </ul>
 </template>
 <script>
     export default {
+        props: {
+            pokedexData: {
+                type: Array,
+            }
+        }
         
     }
 </script>
