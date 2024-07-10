c2ebb3554321def32b2a412c906e6dc0306b1b36
TLLRSK <tullarisk@gmail.com>
2024-07-10 11:36:25 +0200

    add: basic styling viewToggler (non functional yet)

--- src/components/ViewToggler.vue
+++ src/components/ViewToggler.vue
@@ -1,16 +1,29 @@
 <template>
-    <div>
-        <label for="grid">
-            <grid/>
-            <input type="radio" id="grid" name="viewType" value="grid">
-            <span>Grid</span>
-        </label>
-        
-        <label for="list">
-            <list/>
-            <input type="radio" id="list" name="viewType" value="list">
-            <span>View</span>
-        </label>
+    <div class="flex justify-end gap-3">
+        <span>View:</span>
+        <div class="flex gap-3">
+            <label for="grid" class="flex items-center gap-1">
+                <grid class="w-3 h-3"/>
+                <input 
+                    class="hidden"
+                    type="radio" 
+                    id="grid" 
+                    name="viewType" 
+                    value="grid">
+                <span>Grid</span>
+            </label>
+            
+            <label for="list" class="flex items-center gap-1">
+                <list class="w-3 h-3"/>
+                <input 
+                    class="hidden"
+                    type="radio" 
+                    id="list" 
+                    name="viewType" 
+                    value="list">
+                <span>View</span>
+            </label>
+        </div>
     </div>
 </template>
 <script>
