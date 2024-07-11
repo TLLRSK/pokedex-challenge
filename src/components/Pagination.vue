7c115d562eca5b871329b448c1181b1b34c743e5
TLLRSK <tullarisk@gmail.com>
2024-07-11 13:49:04 +0200

    add: prev and next page buttons working

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex gap-3">
-        <button>Prev</button>
+        <button @click="pagePrev" :disabled="disabledPrev">Prev</button>
         <button 
             v-for="page in pages" 
             :key="page"
             @click="changePage(page)">
             {{page}}
         </button>
-        <button>Next</button>
+        <button @click="pageNext" :disabled="disabledNext">Next</button>
     </div>
 </template>
 <script>
@@ -16,12 +16,30 @@
             pages: {
                 type: Number,
                 required: true,
+            },
+            currentPage: {
+                type: Number,
+                required: true,
             }
+        },
+        computed: {
+            disabledPrev() {
+                return this.currentPage === 1;
+            },
+            disabledNext() {
+                return this.currentPage === this.pages;
+            },
         },
         methods: {
             changePage(page) {
                 this.$emit('page-changed', page)
-            }
+            },
+            pagePrev() {
+                this.changePage(this.currentPage - 1);
+            },
+            pageNext() {
+                this.changePage(this.currentPage + 1);
+            },
         }
     }
 </script>
