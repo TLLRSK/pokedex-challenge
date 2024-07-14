33fabdb85354a5468f9a77896289cec8030e776f
TLLRSK <tullarisk@gmail.com>
2024-07-14 15:09:10 +0200

    add: closing menu on clicking outside

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,42 +1,41 @@
 <template>
   <nav class="nav">
-    <label for="menu-toggler" :class="getMenuTogglerClasses">
-      <input
-        class="absolute appearance-none"
-        type="checkbox"
-        name="menu-toggler"
-        id="menu-toggler"
-        v-model="isChecked"
-      />
+    <button :class="getMenuTogglerClasses" @click="toggleMenu">
       <menu-icon />
-    </label>
+    </button>
 
-    <ul :class="getLinkListClasses">
+    <ul ref="linkList" :class="getLinkListClasses">
       <li class="ml-auto">
-        <router-link class="text-main font-semibold uppercase" to="/">Home</router-link>
+        <router-link class="text-main font-semibold uppercase" to="/" @click="closeMenu">Home</router-link>
       </li>
       <li class="ml-auto">
-        <router-link class="text-main font-semibold uppercase" to="/favs">Favs</router-link>
+        <router-link class="text-main font-semibold uppercase" to="/favs" @click="closeMenu">Favs</router-link>
       </li>
     </ul>
+    <div v-if="isChecked" class="overlay fixed top-[0] right-[0] bottom-[0] left-[0] -z-[10]"></div>
   </nav>
 </template>
 
 <script>
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { MenuIcon } from "../util/index.js";
+
 export default {
   components: {
     MenuIcon,
   },
   setup() {
     const isChecked = ref(false);
+
+    const linkList = ref(null);
+
     const getMenuTogglerClasses = computed(() => {
       return [
         "flex m:hidden rounded-full",
         {"bg-white text-gray-dark": isChecked.value}
-      ]
-    })
+      ];
+    });
+
     const getLinkListClasses = computed(() => {
       return [
         {
@@ -44,9 +43,44 @@ export default {
           'hidden m:flex': !isChecked.value
         },
         "m:flex-row absolute m:static left-[0] right-[0] bg-gray-mid m:bg-transparent mt-1 m:mt-[0px] p-2 m:p-[0] gap-3",
-      ]
-    })
-    return {isChecked, getMenuTogglerClasses, getLinkListClasses};
-  }
+      ];
+    });
+
+    const toggleMenu = () => {
+      isChecked.value = !isChecked.value;
+    }
+    const closeMenu = () => {
+      isChecked.value = false;
+    };
+    
+    watch(
+      () => isChecked.value,
+      (newValue) => {
+        if (newValue) {
+          setTimeout(() => {
+            document.addEventListener("click", outsideClickHandler);
+          }, 100);
+        } else {
+          document.removeEventListener('click', outsideClickHandler);
+        }
+      }
+    );
+    
+    const outsideClickHandler = (e) => {
+      const target = e.target;
+      if (!linkList.value.contains(target)) {
+        closeMenu();
+      }
+    };
+
+    return {
+      isChecked,
+      getMenuTogglerClasses,
+      getLinkListClasses,
+      linkList,
+      toggleMenu,
+      closeMenu,
+    };
+  },
 };
 </script>
