23c96388a19ae953266ad99275456b1c734ee6b0
TLLRSK <tullarisk@gmail.com>
2024-07-14 12:57:48 +0200

    add: menu toggler working

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,32 +1,46 @@
 <template>
-  <nav>
-    <label for="menu-toggler" class="m:hidden">
+  <nav class="nav">
+    <label for="menu-toggler" class="menu-toggler flex m:hidden rounded-full">
       <input
         class="absolute appearance-none"
         type="checkbox"
         name="menu-toggler"
         id="menu-toggler"
+        v-model="isChecked"
       />
-
       <menu-icon />
     </label>
 
-    <ul class="hidden m:flex m:gap-3">
-      <li>
-        <router-link to="/">Home</router-link>
+    <ul :class="getNavClasses">
+      <li class="ml-auto">
+        <router-link class="text-main font-semibold uppercase" to="/">Home</router-link>
       </li>
-      <li>
-        <router-link to="/favs">Favs</router-link>
+      <li class="ml-auto">
+        <router-link class="text-main font-semibold uppercase" to="/favs">Favs</router-link>
       </li>
     </ul>
   </nav>
 </template>
 
 <script>
+import { computed, ref } from 'vue';
 import { MenuIcon } from "../util/index.js";
 export default {
   components: {
     MenuIcon,
   },
+  setup() {
+    const isChecked = ref(false);
+    const getNavClasses = computed(() => {
+      return [
+        {
+          'flex flex-col': isChecked.value,
+          'hidden m:flex': !isChecked.value
+        },
+        "m:flex-row absolute m:static left-[0] right-[0] bg-gray-mid m:bg-transparent mt-1 m:mt-[0px] p-2 m:p-[0] gap-3",
+      ]
+    })
+    return {isChecked, getNavClasses};
+  }
 };
 </script>
--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,6 +1,8 @@
 <template>
-  <header class="flex p-1 gap-2 bg-gray-dark text-main m:p-2">
-    <h1 class="mr-auto uppercase font-semibold">Pokedex</h1>
+  <header class="flex p-1 gap-3 bg-gray-dark text-main m:p-3 fixed left-[0] right-[0] z-30">
+    <router-link class="mr-auto" to="/">
+      <h1 class="uppercase font-semibold">Pokedex</h1>
+    </router-link>
 
     <light-toggler />
 
